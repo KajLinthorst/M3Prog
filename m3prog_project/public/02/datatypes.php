15faac989dddbde6a69fa6fb6f58a6d142862ca6
KajLinthorst <37048@ma-web.nl>
2024-03-07 09:52:29 +0100

    never back down never what?

---
+++ m3prog_project/public/02/datatypes.php
@@ -0,0 +1,11 @@
+    <?php
+
+$huisnummer = 2;
+$geslaagd = true;
+$literprijs = 2.50;
+$geenwaarde = 0;
+
+echo "Mijn huisnummer is $huisnummer, Ik ben geslaagd voor mijn examen: $geslaagd, De literprijs is $literprijs, Dit is niks: $geenwaarde";
+
+
+?>
