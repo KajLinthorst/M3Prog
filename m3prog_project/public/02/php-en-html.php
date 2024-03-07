10af890bf472baa6362b8dbb11d65c1a42ab0753
KajLinthorst <37048@ma-web.nl>
2024-03-07 14:15:37 +0100

    oehoehh

---
+++ m3prog_project/public/02/php-en-html.php
@@ -0,0 +1,26 @@
+
+<?php
+$pageTitle = "PHP SITE";
+$imageUrl = "https://www.lp-catering.nl/wp-content/uploads/appel.jpg";
+$imageAlt = "Appels zijn gezond";
+$githubName = "KajLinthorst"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=	, initial-scale=1.0">
+    <title><?=$pageTitle?></title>
+</head>
+<body>
+<h1><?=$pageTitle?></h1>
+<img src="<?=$imageUrl?>" alt="<?=$imageAlt?>">
+
+</body>
+<section>
+
+    Auteur: <?=$githubName?>
+
+</section>
+</html>
