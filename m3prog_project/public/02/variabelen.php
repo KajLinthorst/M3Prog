88e9b33187806f683c483b04bf18d90e1a3638a9
KajLinthorst <37048@ma-web.nl>
2024-03-07 14:06:43 +0100

    02 variabelen

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,16 @@
+    <?php
+
+$auto2 = "dit is een 2de auto";
+$_naam = "Kaj";
+//$auto's = "werkt niet";
+//$auto/onderdelen = "dit werkt ook niet";
+//$huis&tuin = "womp womp werkt niet";
+$naw = "naw man i work";
+//$22 = "nuhuh werkt";
+//$$auto = "autowerktniet";
+$kleurVanAuto = "wahoo hij werkt";
+//$software-dev = "software dev werkt";
+
+
+
+?>
