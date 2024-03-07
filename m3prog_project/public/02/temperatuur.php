856a08a48ac7d38a9f9f6388019bb7123bef64a2
KajLinthorst <37048@ma-web.nl>
2024-03-07 14:29:11 +0100

    tempratuur poe poe wat warm

---
+++ m3prog_project/public/02/temperatuur.php
@@ -0,0 +1,24 @@
+    <?php
+// Fahrenheit naar Celsius
+$fahrenheit = 74;
+$celsius = ($fahrenheit -32) / 1.8;
+echo"{$fahrenheit} graden Fahrenheit = {$celsius} graden Celsius. <br/>";
+
+// Celsius naar Fahrenheit
+$celsius = 23;
+$fahrenheit = ($celsius +32) /1.8;
+echo "{$celsius} graden Celsius = {$fahrenheit} graden Fahrenheit. <br/>";
+
+// Celsius naar Kelvin
+$celsius = 25;
+$kelvin = $celsius +30;
+echo"{$celsius} graden celcius = {$kelvin} graden Kelvin. <br/>";
+
+// Fahrenheit naar Kelvin
+$fahrenheit = 74;
+$kelvin = $fahrenheit - 32;
+echo"{$fahrenheit} graden fahrenheit = {$kelvin} graden Kelvin. <br/>";
+
+
+
+?>
